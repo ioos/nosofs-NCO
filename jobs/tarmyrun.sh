14e89a416988706f3123be2b2d99d6dff508cb23
Patrick Tripp <patrick.tripp@rpsgroup.com>
2019-12-09 18:49:32 +0000

    cleanup tar script

--- jobs/tarmyrun.sh
+++ jobs/tarmyrun.sh
@@ -11,20 +11,20 @@ CYC=$2
 COMDIR=/com/nos/cbofs.$CDATE
 
 tarfile=AWS.cbofs.$CDATE.tgz
-tmpdir=/ptmp/$USER/tmp/cbofs.$CDATE
-mkdir -p $tmpdir
+tmproot=/ptmp/$USER/tmp
+tmpdir=AWS.cbofs.$CDATE
+mkdir -p $tmproot/$tmpdir
 
 
 hhlist='01 06 12 18 24 48'
 
 for hh in $hhlist
 do
-  cp -p $COMDIR/nos.cbofs.fields.f0$hh.$CDATE.t${CYC}z.nc $tmpdir
+  cp -p $COMDIR/nos.cbofs.fields.f0$hh.$CDATE.t${CYC}z.nc $tmproot/$tmpdir
 done
 
-cd $tmpdir
-cd ..
-tar -czvf $tarfile cbofs.$CDATE/
+cd $tmproot
+tar -czvf $tarfile $tmpdir/
 
-rm -Rf $tmpdir
+rm -Rf $tmproot/$tmpdir
 
