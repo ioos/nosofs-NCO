b42afbefe822834d470be59d7c7a6e28289eb927
Patrick Tripp <patrick.tripp@rpsgroup.com>
2019-12-09 22:10:48 +0000

    ROMS efa working

--- jobs/launcher.sh
+++ jobs/launcher.sh
@@ -11,13 +11,13 @@ if [ $# -ne 6 ] ; then
 fi
 
 export HOMEnos=/save/nosofs-NCO
-export I_MPI_OFI_LIBRARY_INTERNAL=1
+export I_MPI_OFI_LIBRARY_INTERNAL=0
 export I_MPI_DEBUG=1
 
-export I_MPI_FABRICS=shm:ofi
+#export I_MPI_FABRICS='shm:ofi'
 #export I_MPI_FABRICS=efa
 #export FI_PROVIDER=efa
-export FI_PROVIDER=tcp
+#export FI_PROVIDER=tcp
 #export FI_PROVIDER=sockets
 #export FI_EFA_ENABLE_SHM_TRANSFER=1
 
@@ -40,6 +40,22 @@ echo $HOSTS > $HOSTFILE
 #export mpiopts="-localhost $host1 -nolocal -launcher ssh -genvall "
 #export mpiopts="-localhost $localhost -nolocal -path /ptmp/ngofs.20191030 -bootstrap ssh" 
 
+export NPP=140
+export PPN=36
+
 # The following will launch the job on a non-local cluster, e.g. from a head node
-export MPIOPTS="-np $NPP -ppn $PPN -hosts $HOSTS -nolocal -bootstrap ssh" 
+#export MPIOPTS="-np $NPP -ppn $PPN -hosts $HOSTS -nolocal -bootstrap ssh -iface ens5" 
+#export MPIOPTS="-verbose -np $NPP -ppn $PPN -hosts $HOSTS -nolocal -launcher ssh -iface ens5" 
+
+export MPIOPTS="-nolocal -hosts $HOSTS -launcher ssh -np $NPP -ppn $PPN " 
 ./fcstrun.sh $CDATE $HH
+
+
+#       -ofi   Use this option to select an OpenFabrics Interfaces* (OFI*) capable network fabric.  The
+#              application  attempts  to use an OFI-capable network fabric. If no such fabric is avail‐
+#              able, another fabric from the list tmi,dapl,tcp,ofa is used.
+#
+#       -OFI   Use this option to select an OFI-capable network fabric. The  application  fails  if  no
+#              such fabric is found.
+
+
